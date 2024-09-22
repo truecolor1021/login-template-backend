7213895b04821507bb18438a0f037253cd59b069
TerryHart <true.color1021@gmail.com>
2024-09-22 07:21:20 -0500

    update

--- validation/register.validation.js
+++ validation/register.validation.js
@@ -9,19 +9,6 @@ module.exports = (data) => {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (validator.isEmpty(data.firstName)) {
-    errors.firstName = "FirstName field is required";
-  }
-  if (validator.isEmpty(data.lastName)) {
-    errors.lastName = "LastName field is required";
-  }
-  if (validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!validator.isEmail(data.email)) {
-    errors.email = "Email is invalid!";
-  }
-  if (validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
   }
   if (!validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords do not match!";
