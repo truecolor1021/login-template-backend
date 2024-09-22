a0e09b65f12f91e61e97aacc69cb0649e0becd62
TerryHart <true.color1021@gmail.com>
2024-09-22 07:21:07 -0500

    update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,10 @@
-// // routes/router.js
-// const express = require("express");
-// const router = express.Router();
-// const authController = require("../controllers/authController");
+// routes/router.js
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
 
-// // Define the routes and associate them with controller methods
-// router.post("/register", authController.register);
-// router.post("/login", authController.login);
+// Define the routes and associate them with controller methods
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
-// module.exports = router;
+module.exports = router;
